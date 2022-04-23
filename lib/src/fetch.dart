defc9f675658d6a894a08daa792421b64729e339
Anggoran <muhammad.anggoran@ui.ac.id>
2022-04-23 12:11:23 +0700

    🧰change back to dart:io

--- lib/src/fetch.dart
+++ lib/src/fetch.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart' show MediaType;
 import 'package:mime/mime.dart';
 import 'package:storage_client/src/types.dart';
-import 'package:universal_io/io.dart';
 
 Fetch storageFetch = Fetch();
 

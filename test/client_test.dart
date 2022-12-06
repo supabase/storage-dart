d8dc24d4fc30d817e016de3375e47f9b26f2f41c
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-12-06 14:44:33 +0900

    fix path to the sadcat file

--- test/client_test.dart
+++ test/client_test.dart
@@ -5,7 +5,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:storage_client/src/types.dart';
 import 'package:storage_client/storage_client.dart';
 import 'package:test/test.dart';
-import "package:path/path.dart" show dirname, normalize;
+import "package:path/path.dart" show join;
 
 const storageUrl = 'http://localhost:8000/storage/v1';
 const storageKey =
@@ -39,8 +39,8 @@ void main() {
     // Register default mock values (used by mocktail)
     registerFallbackValue(const FileOptions());
     registerFallbackValue(const FetchOptions());
-    file = File(
-        '${dirname(normalize(Platform.script.toFilePath()))}/fixtures/upload/sadcat.jpg');
+    file = File(join(
+        Directory.current.path, 'test', 'fixtures', 'upload', 'sadcat.jpg'));
   });
 
   test('List files', () async {
